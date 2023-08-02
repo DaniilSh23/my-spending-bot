3696d22a8729675244538f2d70b74472c96b1931
DaniilSh23 <danyasevas11@gmail.com>
2023-08-02 21:54:31 +0300

    изменил файлик авторизации сессии бота

--- bot_authorization.py
+++ bot_authorization.py
@@ -1,12 +1,29 @@
+import asyncio
+import subprocess
+
 from pyrogram import Client
 
-from settings.config import API_ID, API_HASH, TOKEN
+from settings.config import TOKEN, API_ID, API_HASH
 
-app = Client(
-    # "work_bot",
-    'test_bot',
-    api_id=API_ID, api_hash=API_HASH,
-    bot_token=TOKEN
-)
+api_id = API_ID
+api_hash = API_HASH
+bot_token = TOKEN
 
-app.run()
+
+# app = Client(
+#     "test_bot",
+#     api_id=api_id, api_hash=api_hash,
+#     bot_token=bot_token
+# )
+#
+# app.run()
+
+async def main():
+    async with Client("test_bot", api_id, api_hash, bot_token=bot_token) as app:
+        rslt = await app.get_me()
+        command = f"echo 'Запущен скрипт получения файла сессии для бота. Результат метода get_me():{rslt}'"
+        process = subprocess.Popen(command, shell=True)
+        process.wait()
+
+
+asyncio.run(main())
